4ec6afc5f72c4baa35bd637d133c13e168ecf9a4
Viet Truong <truong.viet1903@gmail.com>
2024-03-19 21:52:06 -0600

    lab2

--- js/index.js
+++ js/index.js
@@ -1,29 +1,10 @@
-import { getToDoData } from "./lib/firebase/api";
-import { todoItemTemplate } from "./templates/todoItem";
+// index.js
+import { getToDoData } from "./models/toDoListModel";
+import { toDoListView } from "./views/toDoListView";
 
 async function appInit() {
-	const appData = await getToDoData();
-	// interactive templating
-	const toDoItems = Object.values(appData).map((obj)=> {
-		return todoItemTemplate(obj.todo);
-	});
-
-	// toDoItems.forEach(todo =>{
-	// 	document.querySelector('#root').insertAdjacentHTML("afterbegin", todo);
-	// });
-
-	
-	// render function jsx +++ data
-	// template fnc() passing the data ===>> template string htmlish string
-
-	// interactive templating
-	//container
-	const div = document.createElement('div');
-	toDoItems.forEach(markup => {
-		div.appendChild(markup);
-	})
-
-	document.querySelector('main').append(div);
+  getToDoData();
+  toDoListView();
 }
 
 appInit();
